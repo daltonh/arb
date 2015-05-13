774213ade1df439454a74b2e378bfbf3687ebf84
Dalton Harvie <daltonh@unimelb.edu.au>
2015-05-13 15:46:25 +1000

    Minor change to osx install file

--- misc/install_dependencies/install_dependencies_on_osx_using_macports.sh
+++ misc/install_dependencies/install_dependencies_on_osx_using_macports.sh
@@ -2,16 +2,15 @@
 
 # this script just installs the macports packages that are required for arb
 # this file is specific to osx
-# run as sudo, as in 'sudo ./install_dependencies_on_osx_using_macports.sh'
-
-# as well as running this file you will also have to:
-# 1) install Xcode with the command line utilities
-# 2) install Xquartz X11 server (I think)
-# 3) install gmsh using their binaries
+# run as sudo, as in 'sudo ./install_dependencies_on_osx_using_macports.sh' after you have:
+# 1) installed Xcode with the command line utilities, including doing `xcode-select --install`
+# 2) installed macports following instructions at https://guide.macports.org/#installing
 
 # other things you might like to do:
+# install gmsh using their binaries from http://geuz.org/gmsh/#Download
 # get syntax highlighting working in vim - see misc/vim_syntax
-# install some linear solvers - see src/contributed/suitesparse, src/contributed/pardiso
+# install some other linear solvers - see (e.g.) src/contributed/pardiso
+# use this with ifort (intel's fortran compiler) - just installing it should allow it as an option
 # install paraview as an alternative post-processor to gmsh
 
 ######################
@@ -42,5 +41,8 @@ echo "WARNING: this link will not work if you have multiple gfortran versions in
 #ln -s /opt/local/bin/gfortran-mp-4.9 /usr/local/bin/gfortran
 ln -s /opt/local/bin/gfortran-mp-* /usr/local/bin/gfortran
 
-echo "INFO: you may now want to install the free suitesparse linear solver umfpack fortran 90 wrapper routine.  If so, cd src/contributed/suitesparse and type make";
-echo "INFO: if you haven't already done so install Xcode with the command line utilities, and the Xquartz x11 server";
+# this is now done differently, and based on the mumfpack routine which is automatically downloaded if required
+#echo "INFO: you may now want to install the free suitesparse linear solver umfpack fortran 90 wrapper routine.  If so, cd src/contributed/suitesparse and type make";
+
+# think that this should have already been installed prior to macports actually working, so the following message is redundant?
+#echo "INFO: if you haven't already done so install Xcode with the command line utilities, and the Xquartz x11 server";
