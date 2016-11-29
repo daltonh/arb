2e5fdd3007768c302cfe0f60ffb83be66117e82c
Dalton Harvie <daltonh@unimelb.edu.au>
2016-11-29 20:55:01 +1100

    Minor bugfix to allow output.stat to be written for a variable that contains no elements

--- src/output_module.f90
+++ src/output_module.f90
@@ -357,7 +357,7 @@ do ntype = 1, ubound(var_types,1)
     m = var_list(var_list_number(centring="all",type=type))%list(nvar)
 
     if (size(var(m)%funk) == 0) then
-      write(textline,fmt=formatline) "variable "//trim(var(m)%type)//' '//trim(var(m)%name)//': contains no elements'
+      write(textline,'(a)') "variable "//trim(var(m)%type)//' '//trim(var(m)%name)//': contains no elements'
     else
     
       max_loc = 1
