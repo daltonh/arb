d7de3ce06b53317956635050b1dbfdb754bc38e4
Dalton Harvie <daltonh@unimelb.edu.au>
2017-02-28 00:07:49 +1100

    Update to bouncing ball batcher example

--- misc/batcher/examples/bouncing_ball/batcher_setup.pm
+++ misc/batcher/examples/bouncing_ball/batcher_setup.pm
@@ -21,9 +21,11 @@ sub simulation_setup {
 # parallel = 0 and pbs = 1 for cluster pbs submission (each job submitted individually to the queue given by $pbs_queue_name below
   $::parallel = 0; # default to run arb jobs in series
 
-  $::prune_output_structure = 0; # clear tmp, src, etc. from final run_* directories
+  $::prune_output_structure = 0; # clear build and other run files from run_dir
 
   $::use_string_variables = 0; # if on uses arb runtime global string variables rather than batcher whole of file replacements (default)
+
+  $::use_previous_build = 1; # if on uses build information from previous run
 
 }
 
