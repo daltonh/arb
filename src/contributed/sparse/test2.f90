af9d1e1a9f7db45973da619bf59e9d7d0e88b513
Dalton Harvie <daltonh@unimelb.edu.au>
2017-07-02 00:50:49 +1000

    Trying to get a test program working that calls the sparse libraries

--- src/contributed/sparse/test2.f90
+++ src/contributed/sparse/test2.f90
@@ -11,24 +11,39 @@ integer element(10)
 double precision rhs(4), solution(4)
 
 interface
+! sfCreate( int *Size, int *Complex, int *Error )
 !function sfCreate(a,b,error) bind(c,name='sfcreate')
-function sfCreate(a,b,error) bind(c) ! by default this will become the c function name sfcreate
-use, intrinsic :: iso_c_binding
-integer(c_int) :: a,b,error
+function sfCreate(size_var,complex_var,error) bind(c) ! by default this will become the c function name sfcreate
+!use, intrinsic :: iso_c_binding
+!integer(c_int) :: size_var,complex_var,error
+integer :: size_var,complex_var,error
 end function sfCreate
 end interface
 
+interface
+! sfGetElement( long *Matrix, int *Row, int *Col )
+function sfGetElement(matrix_var,row,col) bind(c)
+!use, intrinsic :: iso_c_binding
+!integer(c_int) :: matrix_var,row,col
+integer :: matrix_var,row,col
+end function sfGetElement
+end interface
+
 matrix = sfCreate(4,0,error)
-! element(1) = sfGetElement(matrix,1,1)
-! element(2) = sfGetElement(matrix,1,2)
-! element(3) = sfGetElement(matrix,2,1)
-! element(4) = sfGetElement(matrix,2,2)
-! element(5) = sfGetElement(matrix,2,3)
-! element(6) = sfGetElement(matrix,3,2)
-! element(7) = sfGetElement(matrix,3,3)
-! element(8) = sfGetElement(matrix,3,4)
-! element(9) = sfGetElement(matrix,4,3)
-! element(10) = sfGetElement(matrix,4,4)
+matrix = sfCreate(4,0,error)
+write(*,*) 'done 0'
+write(*,*) 'matrix = ',matrix
+element(1) = sfGetElement(matrix,1,1)
+write(*,*) 'done 1'
+element(2) = sfGetElement(matrix,1,2)
+element(3) = sfGetElement(matrix,2,1)
+element(4) = sfGetElement(matrix,2,2)
+element(5) = sfGetElement(matrix,2,3)
+element(6) = sfGetElement(matrix,3,2)
+element(7) = sfGetElement(matrix,3,3)
+element(8) = sfGetElement(matrix,3,4)
+element(9) = sfGetElement(matrix,4,3)
+element(10) = sfGetElement(matrix,4,4)
 ! call sfClear(matrix)
 ! call sfAdd1Real(element(1), 2d0)
 ! call sfAdd1Real(element(2), -1d0)
