3c14d7be9d99b4228f0cd85f8dbb737bc6c90215
Dalton Harvie <daltonh@unimelb.edu.au>
2017-09-04 22:15:53 +1000

    Changes to inline code and css files

--- doc/install/install_dependencies_on_osx_using_macports.sh
+++ doc/install/install_dependencies_on_osx_using_macports.sh
@@ -6,3 +6,9 @@
 # eventually this may be integrated with the main install files
 
 port install pandoc haskell-platform
+
+# install vimhl
+# https://github.com/lyokha/vim-publish-helper#customizing-vim-settings
+# cabal update
+# cabal install cabal-install
+# cabal install pandoc-vimhl
