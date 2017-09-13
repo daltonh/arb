c9390d4f061245e32d1f115bac30874832257164
Dalton Harvie <daltonh@unimelb.edu.au>
2017-09-13 13:01:38 +1000

    Red Hat install file added, but not tested

---
+++ install/install_dependencies_on_redhat.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# script installs just the packages that are required for arb
+# this file is specific to redhat, tested on Red Hat Enterprise Linux Server 7.4 (Maipo) as of 13/9/17
+# run as sudo
+
+# WARNING: this script not tested extensively as we generally develop on ubuntu
+
+# ref use hostnamectl and grep on red hat to check correct OS
+
+# os_type=`lsb_release -si`;
+# if [ $os_type != "Ubuntu" ]; then
+#   "ERROR: script must be run on an Ubuntu system"
+#   exit 1
+# fi
+# ubuntu_version=`lsb_release -sr`;
+# echo "INFO: ubuntu_version = $ubuntu_version"
+
+# basic arb dependencies
+yum install maxima
+yum install valgrind
+yum install lapack-devel
+yum install suitesparse-devel suiteparse-static
+yum install perl-Data-Alias
+
+# red hat doesn't have these libraries, so need to copy from ... ??
+# cp /<matplotlibsource>/backend_wx*.py /usr/lib64/python2.7/site-packages/matplotlib/backends
+
+# pre/post-processing - gmsh - more recent binaries are available which are probably worth using instead of the ubuntu packaged version
+#yum install gmsh
+echo "INFO: no longer installing the ubuntu packaged version of gmsh, but you can easily do this via 'sudo yum install gmsh'.  The better alternative is to download a binary directly from the gmsh website."
+# pre/post-processing - paraview
+yum install paraview
+
+# for Lachlan's track script
+yum install python-pip python-devel python-matplotlib
+yum install wxPython wxPython-devel
+pip install --upgrade numpy
+pip install --upgrade pandas
+pip install --upgrade numexpr
+pip install cycler
+
+#echo "INFO: you may now want to install the free suitesparse linear solver umfpack fortran 90 wrapper routine.  If so, cd src/contributed/suitesparse and type make";
+echo "INFO: you could now add the arb/bin directory to your path using one of the include_path.*sh scripts";
